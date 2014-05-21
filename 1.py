ed0d647c11311c0e07dadcae0de6444a259936a3
Chris Angelico <rosuav@gmail.com>
2014-05-22 01:56:21 +1000

    Add stubby (but actually working) viewing of entries

--- 1.py
+++ 1.py
@@ -12,7 +12,25 @@ def get_db():
 @app.route("/")
 def view():
 	db = get_db()
-	return render_template("view.html")
+	query = "select id,date,title,content,publish from one where publish"
+	params = []
+	# TODO: Check query parameters and modify the SQL statement
+	# if authenticated: query += ">= false"
+	# if ?id: query += " and id=%s"; params.append(?id)
+	# if ?search: query += " and (date=%s or title ilike %s or content ilike %s)"; params.extend((?search, "%"+?search+"%", "%"+?search+"%"))
+	more = False; # if ?more: more=True
+	# if ?recent: query += " order by id desc"; else:
+	query += " order by date desc, id desc"
+	if not more: query += " limit 51"
+	cur = db.cursor()
+	cur.execute(query, params)
+	rows = cur.fetchall()
+	morelink = ""
+	if not more and len(rows)>50:
+		rows.pop() # Discard the last row. We only care that it's present (and therefore we need a "More" link)
+		search = "" # ?search
+		morelink = '<p><a href="?search='+search+'&amp;more=1">More...</a></p>'
+	return render_template("view.html", rows=rows, morelink=morelink)
 
 if __name__ == "__main__":
 	import logging
