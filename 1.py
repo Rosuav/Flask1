6040580bdce47b770b2ad5b38ea0f309e79f461f
Chris Angelico <rosuav@gmail.com>
2014-05-22 01:23:59 +1000

    App: Connect to PostgreSQL (not actually used yet)

--- 1.py
+++ 1.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import psycopg2
 app = Flask(__name__)
+
+def get_db():
+	if not hasattr(g, 'pgsql'):
+		# Pull in the actual connection string from a non-git-managed file
+		import config
+		g.pgsql = psycopg2.connect(config.db_connection_string)
+	return g.pgsql
 
 @app.route("/")
 def view():
+	db = get_db()
 	return render_template("view.html")
 
 if __name__ == "__main__":
