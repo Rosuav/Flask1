7db3a8005593b8709f07a206d4c01a3bf9e2ad92
Chris Angelico <rosuav@gmail.com>
2014-05-22 00:28:29 +1000

    Rewrite 1.py to use the templates and now be all my code (for what there is, at least!)

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def view():
+	return render_template("view.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
