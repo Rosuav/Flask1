2e480f76baff75d84c9484b4a7bb2872b5e33560
Chris Angelico <rosuav@gmail.com>
2014-07-13 09:29:44 +1000

    App: Truncate dates to eight digits, thus trimming off any superfluous characters (eg colons)

--- 1.py
+++ 1.py
@@ -96,11 +96,11 @@ def addent():
 		rf = request.form # We're gonna do a lot with this
 		if "id" in rf:
 			cur.execute("update one set date=%s,title=%s,content=%s,publish=%s where id=%s",
-				(rf["date"], rf["title"], rf["content"], rf['action'] == 'Publish', rf["id"]))
+				(rf["date"][:8], rf["title"], rf["content"], rf['action'] == 'Publish', rf["id"]))
 			savenote = '<div class="status"><a href=".?id=' + rf['id'] + '">Updated.</a></div>'
 		else:
 			cur.execute("insert into one (date,title,content,publish) values (%s,%s,%s,%s) returning id",
-				(rf["date"], rf["title"], rf["content"], rf['action'] == 'Publish'))
+				(rf["date"][:8], rf["title"], rf["content"], rf['action'] == 'Publish'))
 			savenote = '<div class="status"><a href=".?id=%d">Inserted.</a></div>' % cur.fetchone()[0]
 	db.commit()
 	return render_template("addent.html", date=date, title=title,
